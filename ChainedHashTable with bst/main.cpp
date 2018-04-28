1c98f1650a195627f09d8ee83d4fc2dbff73aa4c
Illia <36344311+Illia1F@users.noreply.github.com>
2018-04-28 22:15:12 +0200

    Update main.cpp

--- ChainedHashTable with bst/main.cpp
+++ ChainedHashTable with bst/main.cpp
@@ -1,16 +1,18 @@
-#include "BSTree.h"
 #include "hashTable.h"
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    // create the hash table with a size of 5
     HashTable t(5);
+    // insert to the hash table 30 elements
     for(int i=0; i<30; i++)
         t.Insert(i+1);
-
+    // and try to delete some elements
     t.Delete(2);
     t.Delete(12);
+    // and then find some of them
     cout << t.Find(22) << endl;
 
     return 0;
